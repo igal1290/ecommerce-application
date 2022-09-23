9759a072ceeeb7167e0e4965ed86d6dd71c67698
igal1290 <igalhaimovitz@gmail.com>
2022-09-22 21:10:28 -0400

    added styling using css modules into the homepage inside the hero component

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,23 +2,33 @@
 import Link from 'next/link';
 // lib
 import { urlFor } from '../lib/client';
+// css
+import classes from '../styles/Hero.module.css';
 
 const Hero = ({ heroBanner }) => {
   return (
     <div>
-      <div>
-        <p>{heroBanner.smallText}</p>
-        <h3>{heroBanner.midText}</h3>
-        <h1>{heroBanner.largeText1}</h1>
-        <img src={urlFor(heroBanner.image)} alt="headphones" />
-        <div>
+      <div className={classes.showcase}>
+        <div className={classes.content}>
+          <p className={classes.smallText}>{heroBanner.smallText}</p>
+          <h3 className={classes.midText}>{heroBanner.midText}</h3>
+          <h1 className={classes.largeText}>{heroBanner.largeText1}</h1>
           <Link href={`/product/${heroBanner.product}`}>
-            <button type="button">{heroBanner.buttonText}</button>
+            <button className={classes.btn} type="button">
+              {heroBanner.buttonText}
+            </button>
           </Link>
           <div>
-            <h5>Description</h5>
-            <p>{heroBanner.desc}</p>
+            <p className={classes.desc}>{heroBanner.desc}</p>
           </div>
+        </div>
+        <div>
+          <img
+            src={urlFor(heroBanner.image)}
+            alt="headphones"
+            width={400}
+            height={400}
+          />
         </div>
       </div>
     </div>
