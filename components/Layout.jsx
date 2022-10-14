e62eb62694d1a0457d1505e14581a255e355e437
igal1290 <igalhaimovitz@gmail.com>
2022-10-14 12:38:00 -0400

    create and add Layout to the main app component

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,5 +1,22 @@
-const Layout = () => {
-  return <div>Layout</div>;
+// next
+import Head from 'next/head';
+import { Navbar, Footer } from '../components';
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <Head>
+        <title>Over The Top | Sneakers Ecommerce Store</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main>{children}</main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 };
 
 export default Layout;
