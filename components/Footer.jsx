6a9c947d58725daa2ef1eb060c4f6f1729f91785
igal1290 <igalhaimovitz@gmail.com>
2022-10-14 12:42:16 -0400

    create, style and add footer component to layout

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,7 @@
+import classes from '../styles/Footer.module.css';
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return <div className={classes.footer}>&copy;2022 Over The Top</div>;
 };
 
 export default Footer;
